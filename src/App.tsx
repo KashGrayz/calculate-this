70b8e89309562b7d44d81bf0afa4bf3a4b7c51a9
Kash Grays <kasharsgrays93@.com>
2023-08-14 14:50:28 -0400

    Input and Handleclick resources completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from "react";
+import Calculator from './Components/Calculator/Calculator'
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <Calculator />
     </div>
+
   );
+  
 }
 
 export default App;
--- src/Components/Calculator/Calculator.tsx
+++ src/Components/Calculator/Calculator.tsx
@@ -1,9 +1,19 @@
 import React, { useState } from 'react';
 
-const Calculator = (props) => {
+//typescript is weird
+const Calculator: React.FC = () => {
+
+    // Setting input values
+    const [input, setInput] = useState('');
+
+    // Taking input + values
+    const handleButtonClick = (value:string) => {
+        setInput ((prevInput) => prevInput + value);
+    };
+
     return(
         <div>
-            
+
         </div>
 
     );
