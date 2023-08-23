1b892d0ce16d155f28d49b43b81448d80ba2d3ca
Kash Grays <kasharsgrays93@.com>
2023-08-23 17:20:55 -0400

    Buttons + Input Styled

--- src/Components/Calculator/Calculator.tsx
+++ src/Components/Calculator/Calculator.tsx
@@ -24,36 +24,44 @@ const Calculator: React.FC = () => {
 
     return(
         <div className='calculator'>
-            
                 <input
                 className='input'
                 type='text'
                 value={input}
                 readOnly
                 />
-            
             <div className='buttons'>
                 <div>
-                    <button className='button' onClick={clearInput}>C</button>
-                    <button className='button' onClick={() => handleButtonClick('+/-')}>+/-</button>
-                    <button className='button' onClick={() => handleButtonClick('%')}>%</button>
-                    <button className='button' onClick={() => handleButtonClick('/')}>/</button>
-                    <button className='button' onClick={() => handleButtonClick('7')}>7</button>
-                    <button className='button' onClick={() => handleButtonClick('8')}>8</button>
-                    <button className='button' onClick={() => handleButtonClick('9')}>9</button>
-                    <button className='button' onClick={() => handleButtonClick('*')}>x</button>
-                    <button className='button' onClick={() => handleButtonClick('4')}>4</button>
-                    <button className='button' onClick={() => handleButtonClick('5')}>5</button>
-                    <button className='button' onClick={() => handleButtonClick('6')}>6</button>
-                    <button className='button' onClick={() => handleButtonClick('-')}>-</button>
-                    <button className='button' onClick={() => handleButtonClick('3')}>3</button>
-                    <button className='button' onClick={() => handleButtonClick('2')}>2</button>
-                    <button className='button' onClick={() => handleButtonClick('1')}>1</button>
-                    <button className='button' onClick={() => handleButtonClick('+')}>+</button>
-                    <button className='button' onClick={() => handleButtonClick('0')}>0</button>
-                    <button className='button' onClick={() => handleButtonClick('.')}>.</button>
-                    <button className='button' onClick={handleBackButtonClick}>&lt;</button>
-                    <button className='button' onClick={() => handleButtonClick('=')}>=</button>
+                    <div>
+                        <button className='button' onClick={clearInput}>C</button>
+                        <button className='button' onClick={() => handleButtonClick('+/-')}>+/-</button>
+                        <button className='button' onClick={() => handleButtonClick('%')}>%</button>
+                        <button className='button' onClick={() => handleButtonClick('/')}>/</button>
+                    </div>
+                    <div>
+                        <button className='button' onClick={() => handleButtonClick('7')}>7</button>
+                        <button className='button' onClick={() => handleButtonClick('8')}>8</button>
+                        <button className='button' onClick={() => handleButtonClick('9')}>9</button>
+                        <button className='button' onClick={() => handleButtonClick('*')}>x</button>
+                    </div>
+                    <div>
+                        <button className='button' onClick={() => handleButtonClick('4')}>4</button>
+                        <button className='button' onClick={() => handleButtonClick('5')}>5</button>
+                        <button className='button' onClick={() => handleButtonClick('6')}>6</button>
+                        <button className='button' onClick={() => handleButtonClick('-')}>-</button>
+                    </div>
+                    <div>
+                        <button className='button' onClick={() => handleButtonClick('3')}>3</button>
+                        <button className='button' onClick={() => handleButtonClick('2')}>2</button>
+                        <button className='button' onClick={() => handleButtonClick('1')}>1</button>
+                        <button className='button' onClick={() => handleButtonClick('+')}>+</button>
+                    </div>
+                    <div>
+                        <button className='button1' onClick={() => handleButtonClick('0')}>0</button>
+                        <button className='button' onClick={() => handleButtonClick('.')}>.</button>
+                        <button className='button' onClick={handleBackButtonClick}>&lt;</button>
+                        <button className='button2' onClick={() => handleButtonClick('=')}>=</button>
+                    </div>
                 </div>
             </div>
         </div>
